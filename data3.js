4619d6a83a7ad87918835d1fb2a9a9156e88e1b4
vijayanagalakshmi <vijayanagalakshmi14@gmail.com>
2023-05-24 19:47:08 -0400

    react server files

---
+++ data3.js
@@ -0,0 +1,7 @@
+const loans =[ 
+              { id : 1, loanType : "Home loan"},
+              { id : 2, loanType : "Auto loan"},
+              { id : 3, loanType : "Study loan"},
+              { id : 4, loanType : "Business loan"}]
+
+              export default loans;
