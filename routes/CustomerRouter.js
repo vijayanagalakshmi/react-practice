0014150fb54edd687149dce97626c2860630efb9
vijayanagalakshmi <vijayanagalakshmi14@gmail.com>
2023-06-12 22:50:44 -0400

    edit opration commit

--- routes/CustomerRouter.js
+++ routes/CustomerRouter.js
@@ -9,8 +9,8 @@ CustomerRouter.get('/info', async (request, response) => {
     response.send({ info: "Hi There" })
 })
 CustomerRouter.get('/customerSeed', async (request, response) => {
-   
-   try {
+
+    try {
         await Customer.insertMany(customers);
         response.send("Records inserted successfully.");
     } catch (err) {
@@ -22,37 +22,58 @@ CustomerRouter.get('/customers', async (request, response) => {
     const allCustomers = await Customer.find();
     response.send(allCustomers);
 })
-CustomerRouter.get('/customer/:id', async(request,response) =>{
-    try{
-        const cust = await Customer.findOne({_id:request.params.id});
+CustomerRouter.get('/customer/:id', async (request, response) => {
+    try {
+        const cust = await Customer.findOne({ _id: request.params.id });
         console.log(cust);
         response.send(cust);
-    }catch(err){
-        response.send({message:'failed to search the customer record'});
+    } catch (err) {
+        response.send({ message: 'failed to search the customer record' });
     }
 })
 
-CustomerRouter.delete('/customer/:id', async(request,response) =>{
-    try{
-        const deletedCustomer = await Customer.findOneAndDelete({_id:request.params.id});
-        
-        response.send({message: ' Customer with id ' + request.params.id + 'deleted'});
-    }catch(err){
-        response.send({message:'failed to delete the customer record'});
+CustomerRouter.delete('/customer/:id', async (request, response) => {
+    try {
+        const deletedCustomer = await Customer.findOneAndDelete({ _id: request.params.id });
+        console.log("delete customer id", request.params.id);
+
+        response.send({ message: ' Customer with id ' + request.params.id + 'deleted' });
+    } catch (err) {
+        response.send({ message: 'failed to delete the customer record', err });
     }
 })
 
-CustomerRouter.post('/create', async (req, resp) => {
+CustomerRouter.put('/customer/:id', async (request, response) => {
+    //model.findOneAndUpdate(filter, update)
+    console.log('modify customer');
+    const cust = request.body;
+    const filter = { _id: request.params.id }
+    const update = {
+        name: cust.cName,
+        email: cust.email,
+        password: cust.password,
+        cellNo: cust.cellNo,
+        isActiveUser: cust.isActiveUser
+    }
+    try {
+        const modCust = await Customer.findOneAndUpdate(filter, update);
+        response.send(modCust);
+    } catch (err) {
+        response.send({ message: 'Failed to delete customer' });
+    }
+})
+
+CustomerRouter.post('/customer/create', async (req, resp) => {
     console.log('req body:', req.body);
     const cust = req.body;
     try {
-        const createdCustomer = await Customer.create({ cName: cust.cName, email: cust.email ,cellNo: cust.cellNo});
+        const createdCustomer = await Customer.create({ cName: cust.cName, email: cust.email, cellNo: cust.cellNo });
         console.log('created customer:', createdCustomer);
         resp.send(createdCustomer);
     } catch (err) {
         console.log("error", err);
         resp.send({ message: 'Failed to create customer' });
-        
+
     }
 })
 
